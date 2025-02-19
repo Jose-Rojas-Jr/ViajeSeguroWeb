cb6aa1e61b675f43d983aa714e04253039c38eac
Jose-Rojas-Jr <jr.joss1999@gmail.com>
2025-02-19 08:42:32 -0400

    despliegue web

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './components/Home';
 import Contact from './components/Contact';
-
+import firebaseApp from "./firebase";
 function App() {
   return (
     <Router>
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+// Importa la función para inicializar Firebase
+import { initializeApp } from "firebase/app";
+
+// Configuración de Firebase de tu aplicación
+const firebaseConfig = {
+  apiKey: "AIzaSyC5_8b6cSUHpIlymQhZcL0U7Ft7solngqI",
+  authDomain: "viajeseguroweb.firebaseapp.com",
+  projectId: "viajeseguroweb",
+  storageBucket: "viajeseguroweb.firebasestorage.app",
+  messagingSenderId: "346329716982",
+  appId: "1:346329716982:web:f442df1bc8c566730a74d7"
+};
+
+// Inicializa Firebase
+const app = initializeApp(firebaseConfig);
+
