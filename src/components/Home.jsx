a7e29b26b3b263b83825c3ebb6e7248c56b900f6
Jose-Rojas-Jr <jr.joss1999@gmail.com>
2025-03-05 09:41:54 -0400

    despliegue web9

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,7 +13,8 @@
             <h1>  Tu viaje, más seguro que nunca</h1>
             <p>ViajeSeguro te conecta con conductores confiables para que llegues a tu destino con tranquilidad.</p>
             <a href="https://play.google.com/store/games?hl=es_419">
-  <button>Comenzar</button>
+  <button>Comienza tu viaje seguro ya !!!</button>
+  <p>descarga la aplicacion en la play store</p>
 </a>
 
           </div>
